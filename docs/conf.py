db371c1583c827c9c87acb95ba444bc3cb4c5a70
Chilipp <p.sommer@posteo.de>
2017-04-22 09:41:55 +0200

    Removed html_theme in conf.py
    Try to fix issue
    [2971](https://github.com/rtfd/readthedocs.org/issues/2791#issuecomment-296333827)

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,6 @@ import six
 import subprocess as spr
 from itertools import product
 import warnings
-import sphinx_rtd_theme
 # make sure, psyplot from parent directory is used
 sys.path.insert(0, os.path.abspath('..'))
 import psyplot
@@ -135,16 +134,6 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# Add any paths that contain custom static files (such as style sheets)
-# here, relative to this directory. They are copied after the builtin
-# static files, so a file named "default.css" will overwrite the builtin
-# "default.css".
-html_static_path = ['_static']
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify
 
