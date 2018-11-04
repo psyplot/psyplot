bf73e0dbb846f3a3503016704f4e91f82240be0f
Chilipp <p.sommer@posteo.de>
2018-11-04 21:42:43 +0100

    added channel conda-forge for deploy build

--- ci/deploy_anaconda.sh
+++ ci/deploy_anaconda.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-anaconda -t $CONDA_REPO_TOKEN upload -l $TRAVIS_BRANCH --force $BUILDS
+anaconda -t $CONDA_REPO_TOKEN upload -l $TRAVIS_BRANCH --force "$(ls $HOME/miniconda/conda-bld/${TRAVIS_OS_NAME}-64/*.tar.bz2)"
