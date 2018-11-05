cd51164c2d4f97330114ffec0262f26b80a72feb
Chilipp <p.sommer@posteo.de>
2018-11-05 23:12:19 +0100

    Fixed py3.7 install

--- ci/install_python.ps1
+++ ci/install_python.ps1
@@ -10,7 +10,7 @@ $BASE_URL = "https://www.python.org/ftp/python/"
 
 function DownloadMiniconda ($python_version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
-    if ($python_version -match "3.[456]") {
+    if ($python_version -match "3.[4567]") {
         $filename = "Miniconda3-latest-Windows-" + $platform_suffix + ".exe"
     } else {
         $filename = "Miniconda-latest-Windows-" + $platform_suffix + ".exe"
