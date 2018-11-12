7957605a22bc34f47f96e6d108b0bb6eedf8ecc9
Chilipp <p.sommer@posteo.de>
2018-11-12 03:36:07 +0100

    Deploy tags

--- ci/deploy_anaconda.sh
+++ ci/deploy_anaconda.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-anaconda -t $CONDA_REPO_TOKEN upload -l $TRAVIS_BRANCH --force "$(ls $HOME/miniconda/conda-bld/${TRAVIS_OS_NAME}-64/*.tar.bz2)"
+if [[ $TRAVIS_TAG == "" ]]; then LABEL="-l $TRAVIS_BRANCH"; fi
+anaconda -t $CONDA_REPO_TOKEN upload $LABEL --force "$(ls $HOME/miniconda/conda-bld/${TRAVIS_OS_NAME}-64/*.tar.bz2)"
