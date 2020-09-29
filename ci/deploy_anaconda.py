d641ca831802fe9a509a69b125fac632ac1a16a7
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-09-30 00:18:14 +0200

    Rebuild the recipe without test for upload

--- ci/deploy_anaconda.py
+++ ci/deploy_anaconda.py
@@ -3,23 +3,24 @@ import os
 import re
 import conda_build.api
 import subprocess as spr
-fnames = list(conda_build.api.get_output_file_paths('ci/conda-recipe'))
-py_patt = re.compile('py\d\d')
-repl = 'py' + os.getenv('PYTHON_VERSION').replace('.', '')
-fnames = [py_patt.sub(repl, f) for f in fnames]
+
+recipe_path = 'ci/conda-recipe'
+
 if os.getenv("TRAVIS") == "true":
     branch = os.getenv("TRAVIS_BRANCH")
     on_release = os.getenv("TRAVIS_TAG") != ""
 else:
     branch = os.getenv("APPVEYOR_REPO_BRANCH")
     on_release = bool(os.getenv("APPVEYOR_REPO_TAG_NAME"))
-label = ['-l', branch] if not on_release else []
-token = ['-t', os.getenv('CONDA_REPO_TOKEN')]
 
-print("Uploading via " +
-      " ".join(['anaconda -t *****', 'upload', '--force'] + label + fnames))
+label = ['--label', branch] if not on_release else []
+token = ['--token', os.getenv('CONDA_REPO_TOKEN')]
 
-spr.check_call(
-    ['anaconda'] + token + ['upload', '--force'] +
-    label + fnames
-)
+command = ["conda", "build", "--no-test", "--no-copy-test-source-files"]
+
+spr.check_call(["conda", "config", "--set", "anaconda_upload", "yes"])
+
+print("Building recipe via " +
+      " ".join(command + ["--token *******"] + label + [recipe_path]))
+
+spr.check_call(command + token + label + [recipe_path])
