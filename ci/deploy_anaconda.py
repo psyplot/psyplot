6e949634fc0792bee29da6fbecc5136d34a84b44
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-09-30 22:59:33 +0200

    Add python version to the build

--- ci/deploy_anaconda.py
+++ ci/deploy_anaconda.py
@@ -15,12 +15,13 @@ else:
 
 label = ['--label', branch] if not on_release else []
 token = ['--token', os.getenv('CONDA_REPO_TOKEN')]
+python = ['--python', os.getenv('PYTHON_VERSION')]
 
 command = ["conda", "build", "--no-test", "--no-copy-test-source-files"]
 
 spr.check_call(["conda", "config", "--set", "anaconda_upload", "yes"])
 
 print("Building recipe via " +
-      " ".join(command + ["--token *******"] + label + [recipe_path]))
+      " ".join(command + ["--token *******"] + label + python + [recipe_path]))
 
-spr.check_call(command + token + label + [recipe_path])
+spr.check_call(command + token + label + python + [recipe_path])
