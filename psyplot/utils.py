642beccc48ea4c57b8148c0bf39da3da3808230c
Philipp Sommer <philipp.sommer@hereon.de>
2022-01-06 09:10:36 +0100

    plugin entrypoint compatibility fix for python 3.7

--- psyplot/utils.py
+++ psyplot/utils.py
@@ -23,12 +23,28 @@
 # You should have received a copy of the GNU LGPL-3.0 license
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import sys
 import re
 import six
 from difflib import get_close_matches
 from itertools import chain
 from psyplot.compat.pycompat import OrderedDict, filterfalse
 from psyplot.docstring import dedent, docstrings
+
+
+def plugin_entrypoints(group="psyplot", name="name"):
+    """This utility function gets the entry points of the psyplot plugins"""
+    if sys.version_info[:2] > (3, 7):
+        from importlib.metadata import entry_points
+        try:
+            eps = entry_points(group=group, name=name)
+        except TypeError:  # python<3.10
+            eps = [ep for ep in entry_points().get(group, [])
+                   if ep.name ==  name]
+    else:
+        from pkg_resources import iter_entry_points
+        eps = iter_entry_points(group=group, name=name)
+    return eps
 
 
 class DefaultOrderedDict(OrderedDict):
